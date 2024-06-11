26e518384f77291415641f11baf1157da54fc324
Michał Furgała <83299832+00200200@users.noreply.github.com>
2024-06-11 14:46:37 +0200

    add indexes

--- indexes.sql
+++ indexes.sql
@@ -1,5 +1,19 @@
 -- Czesto bedziemy wyszukiwac klienta na podstawie jego numeru PESEL
 CREATE INDEX idx_customers_pesel ON Customers(PESEL);
 
--- Czesto bedziemy wyszukiwac kary danego klienta
+-- Czesto bedziemy wyszukiwac karty danego klienta
 CREATE INDEX idx_cards_customerid ON Cards(CustomerID);
+
+-- Dla szybszego wyszukiwania urodzin
+CREATE INDEX idx_customers_birthdate ON Customers(BirthDate); 
+x
+-- Dla sprawdzanai salda ujemnego
+CREATE INDEX idx_accounts_balance ON Accounts(Balance); 
+
+CREATE INDEX idx_cards_expirationdate ON Cards(ExpirationDate); 
+
+CREATE INDEX idx_transactionhistory_amount ON TransactionHistory(Amount); 
+
+CREATE INDEX idx_loginhistory_logindate ON LoginHistory(LoginDate);
+
+CREATE INDEX idx_transactionhistory_transactiondate ON TransactionHistory(TransactionDate); 
